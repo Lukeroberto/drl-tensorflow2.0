5e9166540349967faf6a5a5bf14c75e1fc24dd91
luke <lukeroberto213@gmail.com>
2019-06-03 00:38:34 -0400

    Finished DQN implementation, need to debug on gpu

--- dqn/agent.py
+++ dqn/agent.py
@@ -1,42 +1,137 @@
+import random
+from collections import deque
 import numpy as np
+from tqdm import tqdm_notebook as tqdm
 
 import tensorflow as tf
 import tensorflow.keras as keras
+from tensorflow.keras.optimizers import Adam
+
+from model import *
 
 class DQN:
     
-    def __init__(self):
-        pass
-    
-    def train(self):
-        # Initialize Replay buffer
-        # Initialize Q Function
+    def __init__(self, params=None):
         
-        for ep in range(self.num_episodes):
-            state_t = env.reset()
+        self.max_steps = 1000
+        if params:
+            self.gamma, self.epsilon, self.epsilon_min, self.epsilon_decay, self.learning_rate= params
+        
+        else:
+            self.gamma = 0.95    # discount rate
+            self.epsilon = 1.0  # exploration rate
+            self.epsilon_min = 0.01
+            self.epsilon_decay = 0.995
+            self.learning_rate = 0.001
+    
+    def train(self, env, num_episodes, batch_size):
+        self.action_size = env.action_space.n
+        
+        # Initialize Replay buffer
+        self.memory = deque(maxlen=2000)
+        
+        # Initialize Q Function
+        self.q_network = Model(self.action_size)
+        self.q_network.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        
+        # Logging
+        rewards = np.zeros(num_episodes)
+        ep_lengths = np.zeros(num_episodes)
+        
+        pbar = tqdm(range(num_episodes), desc="Episode #: ")
+        for ep in pbar:
             
-            for t in range(max_steps):
+            # Reset env
+            state_t = env.reset()
+            state_t = np.reshape(state_t, (1, state_t.size))
+            done = False
+            while not done:
+                
                 # Choose Action
-                action_t = self._epsilonGreedy(state_t)
+                action_t = self.__act(state_t)
                 
                 # Step environment
-                state_t_prime, reward, done = env.step(action_t)
+                state_t_prime, reward, done, _ = env.step(action_t)
+                state_t_prime = np.reshape(state_t_prime, (1, state_t_prime.size))
                 
                 # Store transition in replay buffer
-                transition = Transition(state_t, action_t, state_t_prime, reward, done)
-                self.replay_buffer.store(transition)
+                self.__remember(state_t, action_t, reward, state_t_prime, done)
                 
                 # Sample a batch from replay buffer
-                batch_x, batch_y = self.replay_buffer.sample()
+                if len(self.memory) > batch_size:
+                    self.__replay(batch_size)
+                    
                 
-                # Learn from experience
-                self._learn(batch_x, batch_y)
-    
-    def test(self):
-        pass
-    
-    def _epsilonGreedy(self, state):
-        pass
-    
-    def _learn(X, Y):
-        pass
+                rewards[ep] += reward
+                ep_lengths[ep] += 1
+                
+                if ep > 0:
+                    pbar.set_description(f"Ave. Reward: {np.mean(rewards[:ep]):.2f}, Epsilon: {self.epsilon:.2f}")
+                
+                # Stop episode if done
+                if done:
+                    break
+                    
+        return rewards, ep_lengths
+                    
+    def test(self, env, num_episodes):
+        # Logging
+        rewards = np.zeros(num_episodes)
+        ep_lengths = np.zeros(num_episodes)
+        
+        pbar = tqdm(range(num_episodes))
+        for ep in pbar: 
+            
+            # Reset env
+            state_t = env.reset()
+            done = False
+            for t in range(self.max_steps):
+                
+                # Choose Action
+                action_t = self.__act(state_t)
+                
+                # Step environment
+                state_t_prime, reward, done, _ = env.step(action_t)
+                
+                rewards[ep] += reward
+                ep_lengths[ep] += 1
+                
+                if ep > 0:
+                    pbar.set_description(f"Ave. Reward: {np.mean(rewards[:ep])}")
+                
+                # Stop episode if done
+                if done:
+                    break
+                    
+                
+        
+        
+        return rewards, ep_lengths
+
+    def __remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def __act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.q_network.predict(state)
+        return np.argmax(act_values[0])  # returns action
+
+    def __replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma *
+                          np.amax(self.q_network.predict(next_state)[0]))
+            target_f = self.q_network.predict(state)
+            target_f[0][action] = target
+            self.q_network.fit(state, target_f, epochs=1, verbose=0)
+        
+        self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
+
+    def load(self, name):
+        self.q_network.load_weights(name)
+
+    def save(self, name):
+        self.q_network.save_weights(name)
--- dqn/model.py
+++ dqn/model.py
@@ -3,21 +3,23 @@ import tensorflow as tf
 import tensorflow.keras.layers as kl
 
 class Model(tf.keras.Model):
-    # Model for DQN
+    # Q network 
     def __init__(self, output_dim, input_dim=None):
         super().__init__('mlp_policy')
         
         self.input_dim = input_dim
         self.output_dim = output_dim
         
-        self.hidden1 = kl.Dense(128, activation='relu')
-        self.hidden2 = kl.Dense(64, activation='relu')
+        # Two Hidden Layers
+        self.hidden1 = kl.Dense(32, activation='relu')
+        self.hidden2 = kl.Dense(32, activation='relu')
+        
+        # Outputs Q Values
         self.q_vals = kl.Dense(self.output_dim, activation='linear')
     
     def call(self, inputs):
-        x = tf.convert_to_tensor(inputs, dtype=tf.float32)
-        
-        hidden = self.hidden1(x)
+        # Forward Pass
+        hidden = self.hidden1(inputs)
         hidden = self.hidden2(hidden)
         q_vals = self.q_vals(hidden)
         
